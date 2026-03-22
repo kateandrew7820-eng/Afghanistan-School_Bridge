d787f40e217b613fddd30e0167e29ea423a4ff6b
kateandrew7820-eng <kateandrew78.20@gmail.com>
2026-03-22 11:41:47 +0000

    comit

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -138,7 +138,7 @@ export default function Demo() {
               onClick={() => selectedRole && handleSelectRole(selectedRole)}
               className="min-w-[200px]"
             >
-              {selectedRole ? 'ورود به داشبرد' : 'مقام را انتخاب کنید'}
+              {selectedRole ? 'ورود به داشبرد' : 'انتخاب مقام'}
             </Button>
             <Button
               size="lg"
